e9b9f99ea3da84af4bd15e9cf2a730c6ad0d48bd
Mavoportentous <ortizmav@gmail.com>
2026-06-04 16:45:13 -0600

    feat: integrate HomeManager component with services in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
+import HomeManager from "@/components/HomeManager";
 // app/page.tsx
 import { getServices } from "@/lib/api";
 // Importamos el wrapper que acabamos de crear
 import ClientParticles from "@/components/ClientParticles";
 
 export default async function Home() {
-   const services = await getServices();
-   
-   return (
-     <main>
-       {/* Usamos el componente de cliente que maneja el ssr: false */}
-       <ClientParticles />
-       
-       {/* ... el resto del código de nuestra página ... */}
-     </main>
-   );
+  const services = await getServices();
+
+  return (
+    <main>
+      {/* Usamos el componente de cliente que maneja el ssr: false */}
+      <ClientParticles />
+
+      <HomeManager services={services} />
+    </main>
+  );
 }
